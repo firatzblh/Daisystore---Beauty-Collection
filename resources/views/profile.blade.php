4f4eb33329dd1ccc4c2009c8ad5a8961aa5436d2
firatzblh <139695621+firatzblh@users.noreply.github.com>
2024-05-03 15:45:41 +0700

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,7 +14,7 @@
                             <div class="text-center">
                                 <img src="{{ Vite::asset('resources/images/profile.jpg') }}" class="rounded img-fluid" alt="Profile Picture"  style="width: 200px; height: 250px; object-fit: cover;">
                                 <h5 class="mt-3">FIRA</h5>
-                                <p class="text-muted">Mahasiswa Sistem Informasi 22'</p>
+                                <p class="text-muted">Information Systems Students '22</p>
                             </div>
                         </div>
                         <div class="col-md-8">
@@ -25,8 +25,8 @@
                                         <li class="list-group-item">Name: <br><i class="bi bi-person"></i> Shafira Daffa Tzabillah</li>
                                         <li class="list-group-item">NIM: <br><i class="bi bi-card-text"></i> 1204220130</li>
                                         <li class="list-group-item">Email: <br><i class="bi bi-envelope"></i> firatzblh.03@gmail.com</li>
-                                        <li class="list-group-item">Domisili: <br><i class="bi bi-geo-alt"></i> Surabaya</li>
-                                        <li class="list-group-item">Institut: <br><i class="bi bi-building"></i> University Telkom Surabaya</li>
+                                        <li class="list-group-item">Live: <br><i class="bi bi-geo-alt"></i> Surabaya</li>
+                                        <li class="list-group-item">College: <br><i class="bi bi-building"></i> University Telkom Surabaya</li>
                                     </ul>
                                 </div>
                             </div>
